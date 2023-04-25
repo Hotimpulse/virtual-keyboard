049b361c1e25112ffc73c0a57215feda2a7fa02f
Lev <hotimpulse@hotmail.com>
2023-04-25 12:57:12 +0700

    init: starting the virtual keyboard project

---
+++ index.js
@@ -0,0 +1,13 @@
+document.body.onload = createKeys;
+
+function createKeys() {
+
+
+    // keys
+    
+    //input
+    
+    const input = document.createElement("input");
+    input.setAttribute("type", "text");
+    document.body.appendChild(input);
+}
